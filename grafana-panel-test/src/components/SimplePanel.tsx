dc4c81023ce7dd3d7584b37fffcfb0210dafc93f
Ka Hung Lee <lee.kahung414@gmail.com>
2023-11-12 20:11:05 -0800

    Updated rendering for test gauge based on circle height

--- grafana-panel-test/src/components/SimplePanel.tsx
+++ grafana-panel-test/src/components/SimplePanel.tsx
@@ -29,29 +29,82 @@ const getStyles = () => {
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme2();
   const styles = useStyles2(getStyles);
-  return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: theme.colors.primary.main }} r={100} />
-        </g>
-      </svg>
+  let color = theme.visualization.getColorByName(options.color);
+  const radii = data.series
+    .map((series) => series.fields.find((field) => field.type === 'number'))
+    .map((field) => field?.values.get(field.values.length - 1));
+  const valueToDisplay: number = radii[0];
+  const circleRadius = Math.min(width, height) / 4;
 
+  // Use fillLevel as percentage fill for circle
+  const fillLevel = valueToDisplay / 100;
+
+  // fillHeight of circle from the rect's perspective, i.e. from top border down
+  // should be (2 * radius * fillLevel) or (diameter * fillLevel)
+  const diameter = 2 * circleRadius;
+  const fillHeight = diameter * fillLevel;
+
+  // fillHeight of the circle from the circle's perspective, i.e. bottom border up
+  // should be (2 * radius - fillHeight) or (diameter - fillHeight)
+  const fillHeightCircle = diameter - fillHeight;
+  const percentageCircleHeight = fillHeightCircle / diameter;
+  // console.log(percentageCircleHeight);
+
+  // Values based off of circle centered within rectangular area with fixed width and height
+  const medianHeight = height / 2;
+  const baselineCircleFromTopBorder = medianHeight - circleRadius;
+
+  const renderCircleHeight = () => {
+    if (percentageCircleHeight > 1) {
+      return baselineCircleFromTopBorder + diameter;
+    } else if (percentageCircleHeight < 0) {
+      return baselineCircleFromTopBorder;
+    } else {
+      return baselineCircleFromTopBorder + fillHeightCircle;
+    }
+  };
+
+  const renderGaugeLevels = () => {
+    if (valueToDisplay >= 100) {
+      return `Warning: Max-limit Reached! ${valueToDisplay}`;
+    } else if (valueToDisplay <= 0) {
+      return `Warning: Min-limit Reached! ${valueToDisplay}`;
+    } else {
+      return String(valueToDisplay);
+    }
+  };
+
+  const renderGaugeStyling = () => {
+    return valueToDisplay >= 100 || valueToDisplay <= 0 ? 'red' : '';
+  };
+
+  return (
+    <div className={cx(styles.wrapper)}>
+      <svg className={styles.svg} width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+        <defs>
+          <clipPath id={`clip-${options.panelId}`}>
+            <rect x="0" y={renderCircleHeight()} width={width} height={diameter} />
+          </clipPath>
+        </defs>
+        <circle
+          cx={width / 2}
+          cy={height / 2}
+          r={circleRadius}
+          fill={color}
+          clipPath={`url(#clip-${options.panelId})`}
+        />
+        <text
+          x={width / 2}
+          y={height / 2 - circleRadius - 20}
+          textAnchor="middle"
+          fill={color}
+          fontSize="16"
+          fontFamily="Open Sans"
+          color={renderGaugeStyling()}
+        >
+          {renderGaugeLevels()}
+        </text>
+      </svg>
       <div className={styles.textBox}>
         {options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
         <div>Text option value: {options.text}</div>
--- grafana-panel-test/src/types.ts
+++ grafana-panel-test/src/types.ts
@@ -1,7 +1,20 @@
 type SeriesSize = 'sm' | 'md' | 'lg';
-
+type CircleColor = 'red' | 'green' | 'blue';
 export interface SimpleOptions {
   text: string;
   showSeriesCount: boolean;
   seriesCountSize: SeriesSize;
+  upperThreshold: number;
+  lowerThreshold: number;
+  textColor: string;
+  alertEnabled: boolean;
+  color: CircleColor;
+  panelId: Number;
 }
+
+// export const defaults: SimpleOptions = {
+//   upperThreshold: 7.5,
+//   lowerThreshold: 6.5,
+//   textColor: 'white',
+//   alertEnabled: true,
+// };
